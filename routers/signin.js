e4de15af150d13af247e0091f82496d96c21e4b9
jinzhan <jinzhan@baidu.com>
2018-04-24 17:30:16 +0800

    优化界面

--- routers/signin.js
+++ routers/signin.js
@@ -12,28 +12,31 @@ router.get('/signin', async(ctx, next) => {
 })
 
 router.post('/signin', async(ctx, next) => {
-    console.log(ctx.request.body)
+    console.log(ctx.request.body);
     let name = ctx.request.body.name;
     let pass = ctx.request.body.password;
 
     await userModel.findDataByName(name)
         .then(result => {
-            let res = result
+            let res = result;
             if (name === res[0]['name'] && md5(pass) === res[0]['pass']) {
-                ctx.body = true
+                ctx.body = JSON.stringify({
+                  errno: 0
+                });
                 ctx.session.user = res[0]['name']
                 ctx.session.id = res[0]['id']
                 console.log('ctx.session.id', ctx.session.id)
                 console.log('session', ctx.session)
                 console.log('登录成功')
             }else{
-                ctx.body = false
+              ctx.body = JSON.stringify({
+                errno: 1
+              });
                 console.log('用户名或密码错误!')
             }
         }).catch(err => {
             console.log(err)
-        })
-
-})
+        });
+});
 
 module.exports = router
